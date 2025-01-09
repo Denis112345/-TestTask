2f12fbde525f6d526f81c144d3cb544480fd2617
Denis112345 <piplison2122@gmail.com>
2025-01-10 00:38:28 +0300

    Working code has been created for data_processing

---
+++ data_processing/main_data_processing.py
@@ -0,0 +1,40 @@
+import psutil
+
+class SystemMonitor:
+    """
+    Класс для мониторинга ресурсов системы (CPU, RAM, ROM)
+    """
+
+    def __init__(self):
+        """
+        Инициализация класса
+        """
+        pass
+
+    def get_system_resorces(self, interval=1) -> dict[str, str]:
+        """
+        Метод получения данных нагрузки системы
+
+        Args:
+            interval: интервал для получения данных нагрузки процессора
+
+        """
+        cpu_percent = psutil.cpu_percent(interval) # Загрузка CPU (процент), интервал 1 секунда
+
+        ram = psutil.virtual_memory()  # Информация об оперативной памяти
+        ram_total = round(ram.total / (1024 ** 3), 1)  # RAM в гигабайтах
+        ram_free = round(ram.available / (1024 ** 3), 1)  # Свободная оперативная память в гигабайтах
+
+        disk = psutil.disk_usage('/')  # Информация о диске, / - корень
+        disk_total = round(disk.total / (1024 ** 3), 1)  # Объем диска в гигабайтах
+        disk_free = round(disk.free / (1024 ** 3), 1) # Свободное дисковое пространство в гигабайтах
+
+        return {
+            'CPU': str(cpu_percent) + '%',
+            'RAM': str(ram_free) + ' ГБ / ' + str(ram_total) + ' ГБ',
+            'ROM': str(disk_free) + ' ГБ / ' + str(disk_total) + ' ГБ',
+        }
+    
+if __name__ == '__main__':
+    sys_monitor = SystemMonitor()
+    print(sys_monitor.get_system_resorces())
--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from UI.main_ui import LoadAnalyzerUI
 
 
 
-# async def run_ui():
-#     window = tk.Tk()
-#     UI = LoadAnalyzerUI(window)
-#     UI.run()
+async def run_ui():
+    window = tk.Tk()
+    UI = LoadAnalyzerUI(window)
+    UI.run()
 
-# async def main():
-#     run_ui_task = asyncio.create_task(run_ui())
+async def main():
+    run_ui_task = asyncio.create_task(run_ui())
 
 window = tk.Tk()
 UI = LoadAnalyzerUI(window)
